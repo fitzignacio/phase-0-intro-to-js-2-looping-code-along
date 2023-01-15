bb4a8a82368020c6ef0894e8c307a251fbd33e1e
Fitz Ignacio <ignacio.fitz@gmail.com>
2023-01-14 21:38:22 -0800

    commit tree 6bb7bc6d0866c16f2c980da0d4a77aba9f6057a7

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let messages = [];
+    for(let name of names) {
+        messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(num) {
+    for(let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
